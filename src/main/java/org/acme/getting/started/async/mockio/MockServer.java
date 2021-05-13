927b835a96939038d639a6ada0d5e08e2060fd1a
arkaprovob <arkaprovo.bhattacharjee@sath.com>
2021-05-14 02:03:10 +0530

    instead of blocking event handler offloaded it to a vert.x blocking call

--- src/main/java/org/acme/getting/started/async/mockio/MockServer.java
+++ src/main/java/org/acme/getting/started/async/mockio/MockServer.java
@@ -1,12 +1,18 @@
 package org.acme.getting.started.async.mockio;
 
+import io.vertx.mutiny.core.Future;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.ext.web.client.WebClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
 
 public class MockServer {
+
+    private static final Logger log = LoggerFactory.getLogger(MockServer.class.getName());
+
     private static final Vertx vertx = Vertx.vertx();
     public final static WebClient client = WebClient.create(vertx);
 
@@ -15,14 +21,23 @@ public class MockServer {
         Random random = new Random();
         vertx.createHttpServer()
                 .requestHandler(req -> {
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
                     if (random.nextBoolean()) {
                         String param = req.getParam("name");
-                        req.response().endAndForget("Hello! there "+param);
+
+                        vertx.executeBlocking(promise -> {
+                            try {
+                                Thread.sleep(3000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            promise.complete("sleeping operation was successful");
+                        },true).subscribe().with(res->{
+                            log.info("output of executeBlocking operation is {}",res);
+                            req.response().endAndForget("Hello! there "+param);
+                        });
+
+
                     } else {
                         req.response().setStatusCode(500).endAndForget("error simulation");
                     }
